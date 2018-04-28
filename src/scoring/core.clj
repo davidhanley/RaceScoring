67c7bb73733896b5d4924c92bcd01eeb60fabb05
David Hanley <dhanley@groupon.com>
2018-04-27 19:02:34 -0500

    reindented

--- src/scoring/core.clj
+++ src/scoring/core.clj
@@ -41,64 +41,64 @@
   (try {:birth-year (- (first race-date) (Integer. age))}
        (catch Exception e nil)))
 
-  ; TODO: flip it when the format is last, first
-  (defn row-to-athlete-result [row race-date]
-    "turn a row into an athlete object"
-    (let [itm (comp string/trim (list-getter row))]
-      (merge {:name (itm 1)}
-             (parse-age-to-birth-year (itm 2) race-date)
-             (to-sex (itm 3))
-             (parse-time-or-nil (itm 4))
-             )))
+; TODO: flip it when the format is last, first
+(defn row-to-athlete-result [row race-date]
+  "turn a row into an athlete object"
+  (let [itm (comp string/trim (list-getter row))]
+    (merge {:name (itm 1)}
+           (parse-age-to-birth-year (itm 2) race-date)
+           (to-sex (itm 3))
+           (parse-time-or-nil (itm 4))
+           )))
 
-  (defn scores [base]
-    "an infinite list of descending scores per rank starting with the base score"
-    (map (fn [i] {:score (* base (/ 5 (+ 5 i)))}) (range)))
+(defn scores [base]
+  "an infinite list of descending scores per rank starting with the base score"
+  (map (fn [i] {:score (* base (/ 5 (+ 5 i)))}) (range)))
 
-  (defn ranking-list
-    "an infinite list of ranks, used for females, males, and overall"
-    [key] (map (fn [i] {key (+ i 1)}) (range)))
+(defn ranking-list
+  "an infinite list of ranks, used for females, males, and overall"
+  [key] (map (fn [i] {key (+ i 1)}) (range)))
 
-  (defn strip-comment
-    "Some lines had # comments, strip those out"
-    [str]
-    (string/trim (first (string/split str #"#"))))
+(defn strip-comment
+  "Some lines had # comments, strip those out"
+  [str]
+  (string/trim (first (string/split str #"#"))))
 
-  (defn to-race-struct [data race-id]
-    (let [itm (fn [i] (string/trim (get (nth data i) 0)))
-          points (Integer. (strip-comment (itm 3)))
-          race-date (map #(Integer. %) (string/split (itm 1) #"-"))
-          racers (map merge (map #(row-to-athlete-result % race-date) (drop 4 data)) (ranking-list :overall-rank))
-          score-list (scores points)
-          sexer (fn [sex rank-key] (doall                   ; force this to happen since it's in a with-file
-                                     (map #(dissoc % :sex)
-                                          (map merge
-                                               (filter (fn [athlete] (= (:sex athlete) sex)) racers)
-                                               score-list
-                                               (ranking-list rank-key)))))
-          ]
-      {:name          (itm 0)
-       :date          race-date
-       :url           (itm 2)
-       :points        points
-       :male-racers   (sexer :male :male-rank)
-       :female-racers (sexer :female :female-rank)
-       :race-id       race-id
-       }))
+(defn to-race-struct [data race-id]
+  (let [itm (fn [i] (string/trim (get (nth data i) 0)))
+        points (Integer. (strip-comment (itm 3)))
+        race-date (map #(Integer. %) (string/split (itm 1) #"-"))
+        racers (map merge (map #(row-to-athlete-result % race-date) (drop 4 data)) (ranking-list :overall-rank))
+        score-list (scores points)
+        sexer (fn [sex rank-key] (doall                     ; force this to happen since it's in a with-file
+                                   (map #(dissoc % :sex)
+                                        (map merge
+                                             (filter (fn [athlete] (= (:sex athlete) sex)) racers)
+                                             score-list
+                                             (ranking-list rank-key)))))
+        ]
+    {:name          (itm 0)
+     :date          race-date
+     :url           (itm 2)
+     :points        points
+     :male-racers   (sexer :male :male-rank)
+     :female-racers (sexer :female :female-rank)
+     :race-id       race-id
+     }))
 
-  (defn load-race-data [filename id]
-    (with-open [in-file (io/reader filename)]
-      (to-race-struct (csv/read-csv in-file) id)))
+(defn load-race-data [filename id]
+  (with-open [in-file (io/reader filename)]
+    (to-race-struct (csv/read-csv in-file) id)))
 
-  (defn process-race-data [filename id]
-    (when (string/ends-with? filename ".csv")
-      (println "processing " filename)
-      (with-open [out-file (io/writer (str filename ".json"))]
-        (binding [*out* out-file]
-          (json/pprint (load-race-data filename id))))))
+(defn process-race-data [filename id]
+  (when (string/ends-with? filename ".csv")
+    (println "processing " filename)
+    (with-open [out-file (io/writer (str filename ".json"))]
+      (binding [*out* out-file]
+        (json/pprint (load-race-data filename id))))))
 
-  (defn process-all-races []
-    (map process-race-data (rest (file-seq (java.io.File. "data"))) (range)))
+(defn process-all-races []
+  (map process-race-data (rest (file-seq (java.io.File. "data"))) (range)))
 
-  (defn races [] (process-all-races))
+(defn races [] (process-all-races))
 
