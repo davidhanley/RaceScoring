9505fdcbf31c9e7ed45249752e06b2b32e45cfd5
David Hanley <dhanley@groupon.com>
2018-05-02 09:58:17 -0500

    tweaks

--- project.clj
+++ project.clj
@@ -2,12 +2,10 @@
   :description "TRUSA scoring system"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.csv "0.1.4"]
-                 ;[clj-time "0.7.0"]
-                 ;[org.clojure/core.memoize "0.5.6"]
                  [org.clojure/data.json "0.2.6"]
                  ]
 
   :aot [scoring.core]
-  :main scoring.core
+  :main scoring.races
 
    )
