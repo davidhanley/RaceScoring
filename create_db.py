317aa54bcce17697583ba77973b288cfd8c8e8c7
David Hanley <dhanley@groupon.com>
2020-02-10 15:14:58 -0600

    reverted date hardcoding

--- create_db.py
+++ create_db.py
@@ -126,8 +126,8 @@ def main():
       dp = [ int(x) for x in pop().strip().split('-') ]
       date = datetime.date( dp[0] , dp[1], dp[2]  )
 
-      #pseudoNow = datetime.date.today()
-      pseudoNow = datetime.date( 2019 , 12 , 31) 
+      pseudoNow = datetime.date.today()
+      #pseudoNow = datetime.date( 2019 , 12 , 31) 
       if date > pseudoNow or date < pseudoNow - datetime.timedelta(365):
         #print "******event is over a year old, skipping" 
         continue 
