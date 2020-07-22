a05da9bddc669ff294a4288707ebf001d95412f7
David Hanley <dhanley@groupon.com>
2020-07-22 15:48:52 -0500

    bolek!

--- src/main/scala/org/davidhanley/racescoring/RaceParser.scala
+++ src/main/scala/org/davidhanley/racescoring/RaceParser.scala
@@ -24,3 +24,43 @@ object RaceParser {
   }
 
 }
+
+object ATH {
+
+  private def RotateRight[K,V](node: Node[K,V]) = { //       (5)            4
+    //       / \           / \
+    //      4   D         /   \
+    //     / \           3     5
+    //    3   C    -->  / \   / \
+    //   / \           A   B C   D
+    //  A   B
+    val L = node.left.left
+    val R = new Node[K,V](node.key, node.value, node.left.right, node.right)
+    val N = new Node[K,V](node.left.key, node.left.value, L, R)
+    N
+  }
+
+  def rotateLeft[K,V](node: Node[K,V]) = {
+    //    (3)               4
+    //    / \              / \
+    //   A   4            /   \
+    //      / \          3     5
+    //     B   5   -->  / \   / \
+    //        / \      A   B C   D
+    //       C   D
+    val L = new Node[K,V](node.key, node.value, node.left, node.right.left)
+    val R = node.right.right
+    val N = new Node[K,V](node.right.key, node.right.value, L, R)
+    N
+  }
+}
+
+case class Node[K, V](key: K, value: V, left: Node[K, V], right: Node[K, V]) {
+  val height = {
+    val lh = if (left == null) 0 else left.height
+    val rh = if (left == null) 0 else right.height
+    Math.max(lh, rh) + 1
+  }
+}
+
+
